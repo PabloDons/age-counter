98282201081bea1a3cdfb4e73d2cf39eea85d194
Omar Saad <omar.saad0705@gmail.com>
2023-10-15 17:19:58 +0200

    feat: made it way more accurate

--- src/main.js
+++ src/main.js
@@ -1,20 +1,36 @@
-import {ageInYears} from "./utils.js"
+import {ageInYears, agingVelocity} from "./utils.js"
 import "./style/style.sass"
+import { getEclipticLongitudalVelocityMillenia } from "./astronomy.js";
+
+function toDateObj(str) {
+    const arr = str.split("-").map(Number);
+    return new Date(arr[0], arr[1]-1, arr[2]);
+}
 
 document.addEventListener("DOMContentLoaded", function(){
-
+    if (!window.localStorage.getItem("birthday")) {
+        window.localStorage.setItem("birthday", "1998-05-07")
+    }
 
     const dateinput = document.getElementById("dateinput")
     const counterspan = document.getElementById("counternum")
-    let datevalue = dateinput.value
+
+    dateinput.value = window.localStorage.getItem("birthday")
     
     dateinput.addEventListener("change", (ev)=>{
-        datevalue = ev.target.value
+        const datevalue = ev.target.value
+        window.localStorage.setItem("birthday", datevalue)
+
     });
     
+    let prevage = 0
+    let prevtime = ageInYears(toDateObj(dateinput.value))
     const step = (timestamp) => {
-        const arr = dateinput.value.split("-").map(Number)
-        counterspan.innerText = ageInYears(arr[0], arr[1]-1, arr[2]).toFixed(10)
+        const currage = ageInYears(toDateObj(dateinput.value))
+        counterspan.innerText = currage.toFixed(9)
+        
+        prevage = currage
+        prevtime = timestamp
         window.requestAnimationFrame(step)
     }
     window.requestAnimationFrame(step)
--- src/utils.js
+++ src/utils.js
@@ -1,30 +1,20 @@
-import { juliandays, Earth } from 'aa-js';
+import { juliandays } from 'aa-js';
+import * as Earth from "./astronomy.js"
 
-export function ageInYears(year, monthIndex, day, now=null) {
-    const startdate = new Date(year, monthIndex, day)
-    const nowdate = new Date();
-
+export function ageInYears(startdate) {
     const startjd = juliandays.getJulianDay(startdate)
     const startcoords = Earth.getEclipticLongitude(startjd)
 
     const nowjd = juliandays.getJulianDay()
     const nowcoords = Earth.getEclipticLongitude(nowjd)
 
-    let yeardiff = (nowdate.getFullYear() - startdate.getFullYear())
     const degreediff = (nowcoords - startcoords)
 
-    const startdate_nowyear = new Date(nowdate.getFullYear(), monthIndex, day)
-    const alongyeardiff = nowdate - startdate_nowyear
+    return (degreediff/360)
+}
 
-    // if degreediff negative while year did not switch
-    if (degreediff < 0 && alongyeardiff > 0) {
-        yeardiff += 1
-    }
-
-    // if year switched while degreediff not netagive
-    if (alongyeardiff < 0 && degreediff >= 0) {
-        yeardiff += 1
-    }
-
-    return yeardiff + (degreediff/360)
+export function agingVelocity(now=null) {
+    const nowjd = juliandays.getJulianDay()
+    const velocity = Earth.getEclipticLongitudalVelocity(nowjd);
+    return velocity / 360;
 }
